602e046f248d076e181eeb6a6095c194d44fa0c8
sangib58 <sangibruse@gmail.com>
2025-04-18 19:07:42 +0600

    readme updated

--- AdminApi/obj/Debug/net8.0/AdminApi.AssemblyInfo.cs
+++ AdminApi/obj/Debug/net8.0/AdminApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fdefb3a945a4f7cd8f89c57dc951c7c342a3f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
